9855d03d6b39ae424c3c8ce26d80477f06efd68e
jimwang <wangzhijie05@beyondsoft.com>
2023-02-20 17:01:46 +0800

    add

---
+++ vcpkgdemo/vcpkgdemo.cpp
@@ -0,0 +1,19 @@
+﻿// vcpkgdemo.cpp : Defines the entry point for the application.
+//
+
+#include "vcpkgdemo.h"
+
+using namespace std;
+
+int main()
+{
+	char str[1024] = "test vcpkg demo";
+	output(str);
+	getchar();
+	return 0;
+}
+
+void output(char* str)
+{
+	cout << str << endl;
+}
---
+++ vcpkgdemo/vcpkgdemo.h
@@ -0,0 +1,10 @@
+﻿// vcpkgdemo.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+void output(char* str);
+
+// TODO: Reference additional headers your program requires here.
